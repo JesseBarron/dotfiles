88270520db666f7d2144b83e0d3cbe7f78f89773
Jesse Barron <jesse.barron@packback.co>
2023-10-15 20:45:54 -0500

    Add lvim configuration

---
+++ lvim/config.lua
@@ -0,0 +1,58 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 2 -- insert 2 spaces for a tab
+vim.opt.relativenumber = true -- relative line numbers
+
+-- Add current date on todo files
+vim.keymap.set('n', '<leader>id', function ()
+	if vim.bo.filetype == 'todo' then
+		vim.cmd("put=system('date')")
+	end
+end)
+
+
+function PrintCWD ()
+  print(vim.fn.getcwd())
+end
+
+
+local dap = require 'dap'
+dap.adapters.php = {
+  type = 'executable',
+  command = 'node',
+  args = { '/home/jesse/vscode-php-debug/out/phpDebug.js' }
+}
+
+dap.configurations.php = {
+  {
+    type = 'php',
+    request = 'launch',
+    name = 'Listen for Xdebug',
+    port = 9003,
+    log = false,
+    -- serverSourceRoot = '/var/www/',
+    -- localSourceRoot = '/home/jesse/questions/backend/app-api',
+    -- localSourceRoot = vim.fn.expand("%:p:h").."/"
+    pathMappings = {
+      ["/var/www"] = "/home/jesse/questions/backend/app-api"
+    }
+  }
+}
+
+-- Plugins
+lvim.plugins = {
+  {
+    'mbbill/undotree',
+    config = function ()
+    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+    end
+  },
+  { 'sindrets/diffview.nvim' },
+  { "nvim-telescope/telescope-dap.nvim" }
+}
+
+
