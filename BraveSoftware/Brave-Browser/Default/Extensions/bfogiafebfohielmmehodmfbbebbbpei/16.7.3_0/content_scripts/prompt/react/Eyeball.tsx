f45e84bc2749477ed5b6e526539abca9b7678b02
Jesse Barron <jessebarrondev@gmail.com>
2024-01-05 20:45:04 -0600

    remove cahce

--- BraveSoftware/Brave-Browser/Default/Extensions/bfogiafebfohielmmehodmfbbebbbpei/16.7.3_0/content_scripts/prompt/react/Eyeball.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-type EyeballProps = {
-  onClick: () => void
-}
-export const Eyeball = ({onClick}: EyeballProps) => {
-  return <div id="eyeball" onClick={onClick}></div>
-}
--- BraveSoftware/Brave-Browser/Default/Extensions/bfogiafebfohielmmehodmfbbebbbpei/16.7.3_0/content_scripts/prompt/react/TwoFactorCode.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-type TwoFactorCodeProps = {
-  validateInput: (e: React.FormEvent<HTMLInputElement>) => void
-}
-export const TwoFactorCode = ({validateInput}: TwoFactorCodeProps) => {
-  return <input type="password" onInput={validateInput}></input>
-}
