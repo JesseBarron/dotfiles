9886a9ae7039ae22b7a9027fbdaf4e05dff1dec2
Jesse Barron <jessebarrondev@gmail.com>
2024-08-20 04:27:10 +0000

    Update scripts

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -5,11 +5,11 @@ DEST=~/.config
 ln -sf $(pwd)/alacritty $DEST
 
 # Delete default lvim config
-LVIM_CONFIG_DIR="$DEST/lvim"
-if test -d $LVIM_CONFIG_DIR; then
-  echo "Deleting default lvim config"
-  rm -rf $LVIM_CONFIG_DIR
-fi
+# LVIM_CONFIG_DIR="$DEST/lvim"
+# if test -d $LVIM_CONFIG_DIR; then
+#   echo "Deleting default lvim config"
+#   rm -rf $LVIM_CONFIG_DIR
+# fi
 
 # ln -sf $(pwd)/lvim $DEST
 ln -sf $(pwd)/nvim $DEST
--- scripts/lvim_install.sh
+++ scripts/lvim_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Installing Lua"
-curl -L -R -O https://www.lua.org/ftp/lua-5.4.7.tar.gz
-tar zxf lua-5.4.7.tar.gz
-cd lua-5.4.7
-make all test
-mv /src/lua /
-sudo ln -s /lua /usr/bin/lua
-echo "Finished installing Lua"
+# echo "Installing Lua"
+# curl -L -R -O https://www.lua.org/ftp/lua-5.4.7.tar.gz
+# tar zxf lua-5.4.7.tar.gz
+# cd lua-5.4.7
+# make all test
+# mv /src/lua /
+# sudo ln -s /lua /usr/bin/lua
+# echo "Finished installing Lua"
 
 # attempt to install nvim
 echo "Installing Neovim"
@@ -22,6 +22,6 @@ sudo mv squashfs-root /
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 bash "Finished installing Neovim"
 
-echo "Installing Lvim"
-LV_BRANCH='release-1.4/neovim-0.9' curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh | bash -s -- --no-install-dependencies
-echo "Finished installing Lvim"
+# echo "Installing Lvim"
+# LV_BRANCH='release-1.4/neovim-0.9' curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh | bash -s -- --no-install-dependencies
+# echo "Finished installing Lvim"
