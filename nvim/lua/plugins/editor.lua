298b00f7f3ba4c35378592b78f766dd33097d83f
Jesse Barron <jessebarrondev@gmail.com>
2024-08-20 05:13:34 +0000

    Add lazyvim configs

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -14,4 +14,20 @@ return {
     { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
     { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
   },
+  {
+    "stevearc/conform.nvim",
+    cmd = { "ConformInfo" },
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        html = { "prettierd", "prettier", stop_after_first = true },
+        php = { "pint" },
+      },
+      default_format_opts = {
+        lsp_format = "fallback",
+      },
+    },
+  },
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,4 +7,26 @@ return {
       end)
     end,
   },
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    opts_extend = { "ensure_installed" },
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shfmt",
+        "phpstan",
+        "php-debug-adapter",
+        "phpactor",
+        "angular-language-server",
+        "lua-language-server",
+        "pint",
+        "prettier",
+        "prettierd",
+        "shfmt",
+        "stylua",
+        "typescript-language-server",
+      },
+    },
+  },
 }
