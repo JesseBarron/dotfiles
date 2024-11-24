9366884c611303aef95a746a0476ec6a5e6b2272
jessebarron <jessebarrondev@gmail.com>
2024-11-23 22:15:06 -0600

    Add orgmode pulgins

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -18,4 +18,19 @@ return {
       })
     end,
   },
+  {
+    "chipsenkbeil/org-roam.nvim",
+    tag = "0.1.1",
+    dependencies = {
+      {
+        "nvim-orgmode/orgmode",
+        tag = "0.3.7",
+      },
+    },
+    config = function()
+      require("org-roam").setup({
+        directory = "~/orgfiles/roam",
+      })
+    end,
+  },
 }
