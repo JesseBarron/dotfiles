43e6bb8aa414d54885037e416d8ca3bcf68696c3
Jesse Barron <jessebarrondev@gmail.com>
2025-02-25 21:57:32 -0600

    Update nvim install script

--- scripts/lvim_install.sh
+++ scripts/lvim_install.sh
@@ -11,16 +11,34 @@
 
 # attempt to install nvim
 echo "Installing Neovim"
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+chmod u+x nvim-linux-x86_64.appimage
+./nvim-linux-x86_64.appimage
 
-./nvim.appimage --appimage-extract
+./nvim-linux-x86_64.appimage --appimage-extract
 
-#exposing nvim globally.
+./nvim-linux-x86_64.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+# Optional: exposing nvim globally.
 sudo mv squashfs-root /
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-bash "Finished installing Neovim"
+nvim
+
+# Optional: exposing nvim globally.
+# sudo mv squashfs-root /
+# sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+# nvim
+# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+# chmod u+x nvim.appimage
+# ./nvim.appimage
+#
+# ./nvim.appimage --appimage-extract
+
+#exposing nvim globally.
+# sudo mv squashfs-root /
+# sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+# bash "Finished installing Neovim"
 
 # echo "Installing Lvim"
 # LV_BRANCH='release-1.4/neovim-0.9' curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh | bash -s -- --no-install-dependencies
